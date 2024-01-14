04105c9cc6e9341467c2d2f0d83b46aa5f652a95
JayceFR <jaycejefferson.vicious@gmail.com>
2024-01-14 22:34:04 +0000

    Added comments

--- src/constants/regexConstants.jsx
+++ src/constants/regexConstants.jsx
@@ -1,2 +1,4 @@
+//Declare the constant regex values 
 export const email_regex_value = /^[a-z0-9A-Z]{2,}@..*\.com$/gm
-export const password_regex_value = /\w{8,}/gm
+export const password_regex_value = /^((?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])){8,}.*$/gm
+// export const password_regex_value = /\w{8,}/gm
